347c87e022e890a33c0e764a3c75eca03ba96544
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-04-03 23:19:44 +0300

    start creating resize function -> create conditions depends on image mime type

--- resize.php
+++ resize.php
@@ -1,5 +1,16 @@
 <?php
 function create_thumbnail($path, $save, $width, $height) {
   $info = getimagesize($path); //get image size and her type
-	$size = array($info[0], $info[1]); //push to array
+  $size = array($info[0], $info[1]); //push to array
+  
+  // depends on extends image call the func
+	if ($info['mime'] == 'image/png') {
+		$src = imagecreatefrompng($path); //create new image from file
+	} else if ($info['mime'] == 'image/jpeg') {
+		$src = imagecreatefromjpeg($path);
+	} else if ($info['mime'] == 'image/gif') {
+ 		$src = imagecreatefromgif($path);
+	} else {
+		return false;
+	}
 }
