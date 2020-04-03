2dac4b0f045c84b7ec8bb1943aaac478a8953d48
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-04-03 23:26:57 +0300

    write uploadFile function

--- fileLoad.php
+++ fileLoad.php
@@ -1,5 +1,30 @@
 <?php
-function uploadFile($destinationFolder, $smallImgFolder) {}
+include 'resize.php';
+
+const ALOWED_SIZE = 3145728; // 3mb
+
+function uploadFile($destinationFolder, $smallImgFolder) {
+  $allowedImageTypes = ['image/gif', 'image/png', 'image/jpeg'];
+
+  $newName = uniqid('img_'). "." . pathinfo($_FILES['imges']['name'])['extension'];
+  $path = $destinationFolder . $newName;
+
+  if($_FILES['imges']['size'] > ALOWED_SIZE){
+    echo "Слишком большой размер файла<br>";
+    return -1;
+  }
+  if(!in_array($_FILES['imges']['type'], $allowedImageTypes)){
+    echo "Не верный формат файла";
+    return -1;
+  }
+
+  if (move_uploaded_file($_FILES['imges']['tmp_name'], $path)) {
+    echo "Файл загружен успешно <br>";
+    create_thumbnail($path, $smallImgFolder . $newName, 200, 110);
+  } else {
+    echo "Ошибка загрузки файла <br>";
+  }
+}
 
 function outputImagesFromDir($bigDir, $smallDir) {}
 ?>
